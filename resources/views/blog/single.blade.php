63fd18365908f6babb7d6cd11a2e830e5340595d
Sobak <msobaczewski@gmail.com>
2018-03-22 05:15:25 +0100

    Note disabled comments

--- resources/views/blog/single.blade.php
+++ resources/views/blog/single.blade.php
@@ -2,4 +2,13 @@
 
 @section('content')
     @include('blog.post')
+
+    <div class="comments-disabled">
+        <h3>Komentarze wyłączone</h3>
+
+        <p>
+            Możliwość komentowania na blogu została wyłączona. Zapraszam do kontaktu na
+            Twitterze, Facebooku lub do poprzez formularz. Do usłyszenia!
+        </p>
+    </div>
 @endsection
