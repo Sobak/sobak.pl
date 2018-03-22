7aa6bcfa38390edfc59d1583888201825eb72369
Sobak <msobaczewski@gmail.com>
2018-03-22 04:59:14 +0100

    Reuse comment form styling for the Contact page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -12,35 +12,27 @@
                 poniższego formularza. Jeżeli z preferujesz kontakt poprzez email,
                 napisz na <span class="email-protect">@</span>
             </p>
-            <div role="form" class="wpcf7" id="wpcf7-f1011-p661-o1" dir="ltr">
-                <div class="screen-reader-response"></div>
-                <form action="{{ route('contact') }}" method="post" novalidate>
-                    <p>
-                        Imię<br />
-                        <input type="text" name="your-name" size="40" aria-required="true" aria-invalid="false">
-                    </p>
-                    <p>
-                        Adres email (wymagane)<br />
-                        <input type="email" name="your-email" size="40" aria-required="true" aria-invalid="false">
-                    </p>
-                    <p>
-                        Temat<br />
-                        <input type="text" name="your-subject" size="40" aria-invalid="false">
-                    </p>
-                    <p>
-                        Treść wiadomości<br />
-                        <textarea name="your-message" cols="40" rows="10" aria-invalid="false"></textarea>
-                    </p>
-                    <p>
-                        Przepisz kod z obrazka:<br />
-                        <img width="72" height="24" alt="captcha" src="http://sobak.pl/wp-content/uploads/wpcf7_captcha/4127110757.png"><br />
-                        <input type="text" name="captcha-126" size="40" autocomplete="off" aria-invalid="false">
-                    </p>
-                    <p>
-                        <input type="submit" value="Wyślij">
-                    </p>
-                </form>
-            </div>
+            <form action="{{ route('contact') }}" method="post" class="form" novalidate>
+                <p class="form-input">
+                    <label for="name">Podpis <span class="required">*</span></label>
+                    <input type="text" name="name" id="name" size="40" required>
+                </p>
+                <p class="form-input">
+                    <label for="email">Email <span class="required">*</span></label>
+                    <input type="email" name="email" id="email" size="40" required>
+                </p>
+                <p class="form-input">
+                    <label for="subject">Temat</label>
+                    <input type="text" name="subject" id="subject" size="40">
+                </p>
+                <p class="form-input">
+                    <label for="message">Wiadomość</label>
+                    <textarea name="message" id="message" cols="45" rows="10" required></textarea>
+                </p>
+                <p>
+                    <input type="submit" value="Wyślij">
+                </p>
+            </form>
         </div>
     </article>
 @endsection
