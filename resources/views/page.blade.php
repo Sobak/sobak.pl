e18f195954f7d646aeef08b22b89bad9b476be1b
Sobak <msobaczewski@gmail.com>
2018-03-27 07:20:20 +0200

    Drop redundant classes

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
 
 @section('content')
-    <article class="page type-page status-publish format-standard hentry">
+    <article class="type-page format-standard hentry">
         <header class="entry-header">
             <h1 class="entry-title">{{ $page->title }}</h1>
         </header>
