33bb2d8b175809ef021736eb7627768db4265f07
Sobak <msobaczewski@gmail.com>
2018-03-28 16:47:51 +0200

    Drop unused classes

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
 
 @section('content')
-    <article class="page type-page status-publish format-standard hentry">
+    <article class="type-page hentry">
         <header class="entry-header">
             <h1 class="entry-title">Kontakt</h1>
         </header>
--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
 
 @section('content')
-    <article class="type-page format-standard hentry">
+    <article class="type-page hentry">
         <header class="entry-header">
             <h1 class="entry-title">{{ $page->title }}</h1>
         </header>
