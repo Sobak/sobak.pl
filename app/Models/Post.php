b299cf210578f3917a2dae6a1123ba8c64aad9fd
Sobak <msobaczewski@gmail.com>
2018-03-31 08:28:32 +0200

    Add support for scheduling content

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,11 +2,24 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
     protected $guarded = ['id'];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope('published', function (Builder $builder) {
+            if (config('content.show_scheduled') === false) {
+                $builder->where('created_at', '<=', Carbon::now()->toDateTimeString());
+            }
+        });
+    }
 
     public function getRouteKeyName()
     {
--- config/content.php
+++ config/content.php
@@ -3,6 +3,8 @@
 return [
     'root' => env('CONTENT_ROOT', storage_path('app/content')),
 
+    'show_scheduled' => env('CONTENT_SHOW_SCHEDULED', false),
+
     'links' => [
         'http://m4tx.pl' => "Blog m4tx'a",
         'http://mrowqa.pl' => "Mrowqa's blog",
