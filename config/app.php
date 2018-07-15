683f95060f23632344907d71677abbfc52ef0d61
Sobak <msobaczewski@gmail.com>
2018-07-15 21:13:11 +0200

    Drop unused service providers

--- config/app.php
+++ config/app.php
@@ -184,8 +184,6 @@ return [
          * Application Service Providers...
          */
         App\Providers\AppServiceProvider::class,
-        // App\Providers\AuthServiceProvider::class,
-        // App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
     ],
