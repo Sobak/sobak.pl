1e8a2934c777d3860188ddab536e9297eb7bcf1a
Sobak <msobaczewski@gmail.com>
2018-07-15 09:56:05 +0200

    Fix incorrect URL on 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -9,7 +9,7 @@
         <div class="entry-content">
             <p>
                 Wygląda na to że w tym miejscu nic nie ma. Użyj menu, skorzystaj z wyszukiwarki lub
-                <a href="{{ route('page', ['contact']) }}">skontaktuj się ze mną</a> jeżeli sądzisz,
+                <a href="{{ route('page', ['kontakt']) }}">skontaktuj się ze mną</a> jeżeli sądzisz,
                 coś faktycznie poszło nie tak.
             </p>
         </div>
