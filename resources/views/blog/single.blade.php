952e8944c276804bc33c82c9d1b58b4a0be1d26e
Sobak <msobaczewski@gmail.com>
2020-04-23 06:14:52 +0200

    Drop note about disabled comments
    They are disabled for so long that noone needs an extra note about that.
    Additionally it doesn't look too good, let's just not have comments like
    many blogs nowadays.

--- resources/views/blog/single.blade.php
+++ resources/views/blog/single.blade.php
@@ -9,14 +9,4 @@
 
 @section('content')
     @include('blog.post')
-
-    <div class="comments-disabled">
-        <h3>Komentarze wyłączone</h3>
-
-        <p>
-            Możliwość komentowania na blogu została wyłączona. Zapraszam do kontaktu na
-            Twitterze, Facebooku lub poprzez formularz, <a href="{{ route('contact') }}">o
-            ten tutaj</a>. Do usłyszenia!
-        </p>
-    </div>
 @endsection
