951eaf1883f12fdcc3bac00f384a0da0de03f341
Maciej Sobaczewski <msobaczewski@gmail.com>
2024-03-28 00:35:18 +0100

    Reorder footer items

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -84,8 +84,8 @@
     <footer class="site-footer">
         <div class="site-info">
             <a href="https://github.com/Sobak/sobak.pl" title="{{ __('app.footer.engine_tooltip') }}">{{ __('app.footer.engine') }}</a>
-            <a href="https://wordpress.com/themes/sorbet/" title="{{ __('app.footer.theme_tooltip') }}">{{ __('app.footer.theme') }}</a>
             <a href="{{ route('page', ['polityka-prywatnosci']) }}">{{ __('app.footer.privacy_policy') }}</a>
+            <a href="https://wordpress.com/themes/sorbet/" title="{{ __('app.footer.theme_tooltip') }}">{{ __('app.footer.theme') }}</a>
         </div>
     </footer>
 
